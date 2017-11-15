4c859be5db5188d8cfc3b4c8b501635009476278
yangdeal <ydeal@msn.com>
2017-11-15 23:11:26 +1100

    Update dsm_rsync.sh

--- dsm_rsync.sh
+++ dsm_rsync.sh
@@ -4,6 +4,7 @@ TARGET_SERVER=${2}
 TARGET_SERVER_PORT=${3}
 TARGET_FOLDER=${4}
 
+
 if [[ $# != 4 ]]; then
     echo "usage ${0} [Source_Folder] [TARGET_FOLDER] [TARGET_SERVER_PORT] [TARGET_FOLDER]"
     exit 1
@@ -12,20 +13,44 @@ fi
 rsync_folder () {
     FOLDER=${1}
     exit_status=1
+    echo "+++++start Folder ${FOLDER} at `date`"
+
     while [[ $exit_status -ne 0 ]] && [[ $exit_status -ne 23 ]]
     #error code 23: Partial transfer due to error
     do
-        date
-        rsync -avz --delete --progress -e "ssh -p ${TARGET_SERVER_PORT}" ${FOLDER} ${TARGET_SERVER}::NetBackup/${TARGET_FOLDER}
+        rsync -avz --stats --human-readable --perms --owner --group --delete --backup --backup-dir=Incremental/`date +%Y-%m-%d.%T` -e "ssh -p ${TARGET_SERVER_PORT}" ${FOLDER} ${TARGET_SERVER}::NetBackup/${TARGET_FOLDER}/`date +%Y-%m`
         exit_status=$?
-        echo +++++ rsync folder ${FOLDER} with exit code: ${exit_status}
+        echo "+++++ rsync folder ${FOLDER} with exit code: ${exit_status}"
         #sleep 2 seconds for ^c if required
         sleep 2
     done
+
+    echo "------End of Folder ${FOLDER} at `date`"
+}
+
+clean_old_folder () {
+    OLD_MONTH=${1}
+    OLD_FOLDER=`date --date="-${OLD_MONTH} month" +%Y-%m`
+    echo "++++++Starting remove old backup files in folder ${TARGET_SERVER}::NetBackup/${TARGET_FOLDER}/${OLD_FOLDER} at `date`"
+
+    TEMP_FOLDER=`mktemp -d`
+    rsync -az --delete --stats --human-readable -e "ssh -p ${TARGET_SERVER_PORT}"  ${TEMP_FOLDER}/ ${TARGET_SERVER}::NetBackup/${TARGET_FOLDER}/${OLD_FOLDER}
+    rmdir ${TEMP_FOLDER}
+
+    echo "------End of clean old folder ${TARGET_SERVER}::NetBackup/${TARGET_FOLDER}/${OLD_FOLDER} at `date`"
 }
 
 rsync_folder /etc
 rsync_folder ${Backup_Folder}
+
+TODAY=`date +%d`
+# only run clean job at 1st of the month
+if [ "${TODAY}" == "1" ]; then
+    # 2 means clean folder older than 1 month
+    clean_old_folder 2
+fi
+
+
 
 
 # rsync error code
